93efd4724dd39ade16d061e9fb82f2fe1ec92b7e
Arpit Gupta <guptarpit1997@gmail.com>
2019-05-25 23:18:10 +0530

    PyGCCXML AST generator

---
+++ ast_generator.py
@@ -0,0 +1,34 @@
+import sys
+
+from pygccxml import utils
+from pygccxml import declarations
+from pygccxml import parser
+
+
+def pygccxml_parser(filename):
+    """
+    Implementation of pygccxml parser
+    """
+    generator_path, generator_name = utils.find_xml_generator()
+
+    # Configure the xml generator
+    xml_generator_config = parser.xml_generator_configuration_t(
+        xml_generator_path=generator_path,
+        xml_generator=generator_name)
+    filename = filename
+    decls = parser.parse([filename], xml_generator_config)
+
+    global_namespace = declarations.get_global_namespace(decls)
+    ns = global_namespace.namespace("gr")
+    return declarations.print_declarations(ns)
+
+
+def main():
+    pygccxml_parser(sys.argv[1])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main()
+    else:
+        raise Exception('Please input only one header file!')
